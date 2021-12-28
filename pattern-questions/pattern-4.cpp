1e26633d17d0a98e969a6f66ce54384048f6d0ce
Asit <86393824+Asit0007@users.noreply.github.com>
2021-12-28 10:08:45 +0530

    Create pattern-4.cpp

---
+++ pattern-questions/pattern-4.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"\nEnter a number: ";
+    cin>>n;
+    int i=1;
+  
+  //loop for columns
+    while(i<=n) {
+      
+        int j=1;
+      
+      //loop for rows
+        while(j<=n) {
+          
+            char c='A'+j-1;
+            cout<<c<<" ";
+            j++;
+          
+        }
+        cout<<"\n";
+        i++;
+    }
+}
