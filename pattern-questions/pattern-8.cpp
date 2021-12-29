4c7a7590bba9770d9e5df26b2026b0021595a0ce
Asit <86393824+Asit0007@users.noreply.github.com>
2021-12-29 20:47:06 +0530

    Update and rename pattern8.cpp to pattern-8.cpp

--- pattern-questions/pattern-8.cpp
+++ pattern-questions/pattern-8.cpp
@@ -7,8 +7,12 @@ int main() {
     cin>>n;
     int i=1;
     char c='A';
+    
+    //loop for columns
     while(i<=n) {
         int j=1;
+        
+        //loop for rows
         while(j<=i) {
             cout<<c<<" ";
             c++;
