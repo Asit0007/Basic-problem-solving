03987bd19ba99ded72f1a06c4fa8a6bcadcb1e02
Asit <86393824+Asit0007@users.noreply.github.com>
2021-12-29 20:45:30 +0530

    Update and rename pattern6.cpp to pattern-6.cpp

--- pattern-questions/pattern-6.cpp
+++ pattern-questions/pattern-6.cpp
@@ -7,8 +7,12 @@ int main() {
     cin>>n;
     int i=1;
     char c;
+    
+    //loop for columns
     while(i<=n) {
         int j=1;
+        
+        //loop for rows
         while(j<=n) {
             c='A'+(i+j-2);
             cout<<(c)<<" ";
