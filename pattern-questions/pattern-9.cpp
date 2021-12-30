a74faf68d743a076a0a35211558f12dfe928f880
Asit <86393824+Asit0007@users.noreply.github.com>
2021-12-30 21:27:37 +0530

    Update and rename pattern9.cpp to pattern-9.cpp

--- pattern-questions/pattern-9.cpp
+++ pattern-questions/pattern-9.cpp
@@ -8,11 +8,13 @@ int main() {
     cin>>n;
 
     int i=1;
-
+    
+    //loop for columns
     while(i<=n) {
 
         int j=1;
         
+        //loops for rows
         while(j<=i) {
             char c='A'+i+j-2;
             cout<<c<<" ";
