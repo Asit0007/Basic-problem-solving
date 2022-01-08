8c78d7432911c685fe30a491a96438395d7cf41e
Asit <86393824+Asit0007@users.noreply.github.com>
2022-01-08 21:08:10 +0530

    Update and rename pattern11.cpp to pattern 11.cpp

--- pattern-questions/pattern 11.cpp
+++ pattern-questions/pattern 11.cpp
@@ -8,11 +8,13 @@ int main() {
     cin>>n;
 
     int i=1;
-
+    //loop for columns
     while(i<=n) {
 
         int j=1;
-        char c='A'+i-1; 
+        char c='A'+i-1;
+        
+        //loop for rows
         while(j<=n) {
             cout<<c<<" ";
             j++;
