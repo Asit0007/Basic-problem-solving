f0f170b26e474709f607d60b8deb2a3045335e00
Asit <86393824+Asit0007@users.noreply.github.com>
2022-01-09 23:24:46 +0530

    Update and rename pattern13.cpp to pattern 13.cpp

--- pattern-questions/pattern 13.cpp
+++ pattern-questions/pattern 13.cpp
@@ -8,13 +8,14 @@ int main() {
     cin>>n;
 
     int i=1;
-
+    
+    //loop for columns
     while(i<=n) {
 
         int j=1;
         char c='*';
         
-
+        //loop for rows
         while(j<=(n-i+1)) {
             cout<<c;
             j++;
