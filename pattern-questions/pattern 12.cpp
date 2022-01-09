43192bf916b4cd1aa6e4c4c0f8c39abab95b9a18
Asit <86393824+Asit0007@users.noreply.github.com>
2022-01-09 23:23:30 +0530

    Update and rename pattern12.cpp to pattern 12.cpp

--- pattern-questions/pattern 12.cpp
+++ pattern-questions/pattern 12.cpp
@@ -9,13 +9,14 @@ int main() {
 
     int i=1;
     int count=n;
-
+    
+    //loop for columns 
     while(i<=n) {
 
         int j=1;
         char c='*';
         
-
+        //loop for rows
         while(j<=n) {
             if (j>=count) {
                 cout<<c;
