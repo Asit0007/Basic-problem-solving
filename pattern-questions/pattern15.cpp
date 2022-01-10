47b2ffbcd6e0104d535b2a9835943ffa241afd86
Asit <86393824+Asit0007@users.noreply.github.com>
2022-01-10 20:27:41 +0530

    Add files via upload

---
+++ pattern-questions/pattern15.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"\nEnter number of rows : ";
+    cin>>n;
+
+    int i=1;
+    int count=1;
+
+    while(i<=n) {
+
+        int j=1;
+
+        while(j<=n) {
+            if (j>=count) {
+                cout<<i;
+            }
+            
+            else {
+                cout<<" ";
+            }
+            
+            j++;
+        }
+        count++;
+        cout<<"\n";
+        i++;
+    }
+}
