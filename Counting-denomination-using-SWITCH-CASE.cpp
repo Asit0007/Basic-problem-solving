02d3efed5050d59c59f2c28ecbe1a781d7648d6f
Asit <86393824+Asit0007@users.noreply.github.com>
2022-01-14 23:54:00 +0530

    Update Counting-denomination-using-SWITCH-CASE.cpp

--- Counting-denomination-using-SWITCH-CASE.cpp
+++ Counting-denomination-using-SWITCH-CASE.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main() {
     int n,ch=0;
+    //taking user input
     cout<<"\nenter the amount : ";
     cin>>n;
     while(n>0) {
         int count=0;
         switch(ch) {
+            //case for 100$ notes
             case 0: 
                 count=n/100;
                 n=n%100;
@@ -16,6 +18,7 @@ int main() {
                     cout<<count<<" no of 100$ notes, "; 
                 }
                 break;
+            //case for 50$ notes
             case 1:
                 count=n/50;
                 n=n%50;
@@ -24,6 +27,7 @@ int main() {
                     cout<<count<<" no of 50$ notes, ";
                 }
                 break;
+            //case for 20$ notes
             case 2:
                 count=n/20;
                 n=n%20;
@@ -32,6 +36,7 @@ int main() {
                     cout<<count<<" no of 20$ notes, ";
                 }
                 break;
+            //case for 1$ notes
             case 3:
                 count=n/1;
                 n=n%1;
