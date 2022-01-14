fb7b966e050b5e81dd6b53ba3173cac2f8ce7d05
Asit <86393824+Asit0007@users.noreply.github.com>
2022-01-14 23:48:41 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int fibonaci(int n) {
+    int a=0;
+    int b=1;
+    int sum=0;
+    for(int i=0;i<n-2;i++) {
+        sum=a+b;
+        a=b;
+        b=sum;
+    }
+    return sum;
+
+}
+
+int main() {
+    int n;
+    cin>>n;
+    int ans = fibonaci(n);
+    cout<<"The "<<n;
+    cout<<"th fibonaci no is : "<<ans<<endl;
+    
+}
